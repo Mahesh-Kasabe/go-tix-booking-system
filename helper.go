3b3b57f4e012277b91956543d6b81a9dfce79688
Mahesh Kasbe <maheshkasbe010@gmail.com>
2022-10-26 21:42:06 +0530

    Last commit

--- helper.go
+++ helper.go
@@ -2,7 +2,7 @@ package main
 
 import "strings"
 
-func validateUserInput(fname string, lname string, email string, tix int) (bool, bool, bool) {
+func ValidateUserInput(fname string, lname string, email string, tix uint) (bool, bool, bool) {
 	isValidname := len(fname) >= 2 && len(lname) >= 2
 	isValidemail := strings.Contains(email , "@")
 	isValidTix := tix > 0 
--- main.go
+++ main.go
@@ -1,16 +1,51 @@
 package main
 
-import "fmt"
+import ("fmt" )
+
+type UserData struct {
+	firstName       string
+	lastName        string
+	email           string
+	tickets uint
+}
+
+var conferenceName string = "Kubecon"
+var remainingTickets uint = 50
+var bookings = make([]UserData , 0)
+
 
 func main(){
 
-	greetusers()
+	greetusers(conferenceName)
 
-	 getusers()
+	firstName, lastName, email, tickets := getusers()
+
+	 isValidname , isValidemail , isValidTix := ValidateUserInput(firstName,lastName,email,tickets)
+
+	 if  isValidname && isValidemail && isValidTix {
+
+		if remainingTickets == 0{
+			fmt.Println("Our Confference is booked out , come back next year ... ")
+		
+
+		booktix(firstName, lastName, email, tickets)
+
+}
+	 }else {
+		if !isValidname {
+			fmt.Println("Your name is incorrect")
+		
+		if !isValidemail {
+			fmt.Println("Your email is incorrect")
+		}}
+		if !isValidTix {
+			fmt.Println("Your ticket is incorrect")
+		}
+	 }
 }
 
-func greetusers() {
-	fmt.Println("Get your kubecon tix here")
+func greetusers(conferenceName string) {
+	fmt.Printf("Get your %v tix here. \n", conferenceName)
 }
 
 func getusers() (string, string, string, uint) {
@@ -32,5 +67,22 @@ func getusers() (string, string, string, uint) {
 	fmt.Scan(&tix)
 
 	return fname , lname , email , tix
+
+}
+
+func booktix(firstName string, lastName string, email string, tickets uint) {
+	remainingTickets = remainingTickets - tickets
+
+	var userdata = UserData  {
+		firstName:       firstName,
+		lastName:        lastName,
+		email:           email,
+		tickets: tickets,
+	}
+
+	bookings = append(bookings, userdata)
+	fmt.Printf("Thanks %v %v for booking %v tickets for kubecon, You will recieve a confirmation email on %v \n", firstName, lastName, tickets, email)
+	fmt.Printf("The list of bookings : %v \n", bookings)
+	fmt.Printf("from 50 Tickets  %v remaining \n", remainingTickets)
 
 }
